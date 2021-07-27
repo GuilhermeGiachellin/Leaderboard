be84924b00bbe520eab787356946784eebc0bd9d
Guilherme Giachellin <81584449+GuilhermeGiachellin@users.noreply.github.com>
2021-07-27 19:21:09 -0300

    Linter correction

--- src/displayHtml.js
+++ src/displayHtml.js
@@ -5,10 +5,10 @@ const generateHTML = () => {
   for (let i = 0; i < 3; i += 1) {
     const li = document.createElement('li');
     counter += 1;
-    li.innerHTML = `<li class="games_li grey"><p>game ${counter}</p></li>`;
+    li.innerHTML = `<li class="games_li"><p>game ${counter}</p></li>`;
     li.classList.add('tasks_li');
     if (counter % 2 !== 0) {
-      li.classList.add('grey_color');
+      li.classList.add('greyColor');
     }
     containerUl.appendChild(li);
   }
